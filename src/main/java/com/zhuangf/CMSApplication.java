8be8bf368ea9edb0bbad0317b59f0b872bf763f7
zhuangf <zhuangf@chinaoly.com>
2018-09-19 19:43:24 +0800

    spring-boot web 启动

--- src/main/java/com/zhuangf/CMSApplication.java
+++ src/main/java/com/zhuangf/CMSApplication.java
@@ -1,12 +1,17 @@
-package com.example.demo;
+package com.zhuangf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 启动类
+ * 扫描同包以及子包
+ * @author chinaoly
+ */
 @SpringBootApplication
-public class DemoApplication {
+public class CMSApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(CMSApplication.class, args);
 	}
 }
