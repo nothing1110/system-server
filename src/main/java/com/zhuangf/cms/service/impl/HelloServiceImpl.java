7069bf280458e58bf3d86f1cf23b8cd2fb0002dc
zhuangf <zhuangf@chinaoly.com>
2018-09-21 15:42:43 +0800

    service层

---
+++ src/main/java/com/zhuangf/cms/service/impl/HelloServiceImpl.java
@@ -0,0 +1,12 @@
+package com.zhuangf.cms.service.impl;
+
+import org.springframework.util.StringUtils;
+
+/**
+ * @author: zhuangf
+ * @description:
+ * @create: 2018-09-20 11:43
+ **/
+public class HelloServiceImpl {
+
+}
