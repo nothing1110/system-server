80c0c381c3b92ff6095dcab79d9aac106174b860
zhuangf <zhuangf@chinaoly.com>
2018-10-08 17:16:34 +0800

    整合myFrame

--- src/main/java/com/zhuangf/cms/dao/LogInfoMapper.java
+++ src/main/java/com/zhuangf/cms/dao/LogInfoMapper.java
@@ -1,11 +1,10 @@
 package com.zhuangf.cms.dao;
 
 import com.zhuangf.cms.model.LogInfo;
+import com.zhuangf.frame.base.dao.MysqlBaseMapper;
 import org.apache.ibatis.annotations.Mapper;
 
 /**
- * TkMyBatis主要使用的是Jpa注解。
- * 如果数据库用下划线命名法，模型类用驼峰命名法，不需要设置，TkMyBatis默认开启转换。
  * @author: zhuangf
  * @create: 2018-09-27 15:18
  **/
